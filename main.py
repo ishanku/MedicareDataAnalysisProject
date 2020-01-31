e0936ba3453e6afd944fc0705c47699803a81789
SSiva Raja <ishanku@gmail.com>
2020-01-31 13:52:16 -0500

    Update Comments

---
+++ main.py
@@ -0,0 +1,22 @@
+import boto
+import boto3
+from boto3 import session
+import io
+import logging
+from botocore.exceptions import ClientError
+from config import awskey
+from config import secret
+from config import bucketname
+
+FileHospitalGeneralData="MedicareDataAnalysis/HospitalData/Hospital General Information.csv"
+
+class DataLoader():
+    def LoadFile(file):
+        s3 = boto3.client('s3',
+        aws_access_key_id=awskey,
+        aws_secret_access_key=secret)
+        # Call S3 to list current buckets
+        response = s3.list_buckets()
+        Object_Read=s3.get_object(Bucket=bucketname,Key=FileHospitalGeneralData)
+        output_data= pd.read_csv(Object_Read['Body'])
+        return output_data
