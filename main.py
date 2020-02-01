38ddf21382561027bbb5b628be13b0e6cad4f00a
SSiva Raja <ishanku@gmail.com>
2020-02-01 10:57:28 -0500

    Code Cleanup

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import boto
+from matplotlib import pyplot as plt
+from scipy.stats import linregress
+import numpy as np
+from sklearn import datasets
+import pandas as pd
 import boto3
 from boto3 import session
 import io
@@ -9,6 +13,11 @@ from config import secret
 from config import bucketname
 
 FileHospitalGeneralData="MedicareDataAnalysis/HospitalData/Hospital General Information.csv"
+FileHosipitalSpendingNationalData="MedicareDataAnalysis/HospitalData/Medicare Hospital Spending Per Patient - National.csv"
+FilePhysicianData="MedicareDataAnalysis/HospitalData/Hospital General Information.csv"
+FilePhysicianNationalData="MedicareDataAnalysis/PhysicianData/Physician_Compare_National_Downloadable_File.csv"
+FilePhysicianGroupMIPSData="MedicareDataAnalysis/PhysicianData/Physician_Compare_2017_Group_Public_Reporting-Overall_MIPS_Performance.csv"
+FilePhysicianIndivialMIPSData="MedicareDataAnalysis/PhysicianData/Physician_Compare_2017_Individual_EC_Public_Reporting_-_Overall_MIPS_Performance.csv"
 
 class DataLoader():
 	def LoadFile(file):
@@ -18,14 +27,3 @@ class DataLoader():
 		Object_Read=s3.get_object(Bucket=bucketname,Key=file)
 		output_data= pd.read_csv(Object_Read['Body'])
 		return output_data
-
-class DataLoader1():
-	def LoadFile(file):
-		s3 = boto3.client('s3',aws_access_key_id=awskey,aws_secret_access_key=secret)
-		# Call S3 to list current buckets
-		response = s3.list_buckets()
-		#print (response)
-		Object_Read=s3.get_object(Bucket=bucketname,Key=file)
-		output_data= pd.read_csv(Object_Read['Body'])
-		#return output_data
-		print (output_data.head())
