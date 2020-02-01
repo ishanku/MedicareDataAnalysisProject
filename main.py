f88fdc14c9dd596a1608216c9fb5c08dd24a08ab
SSiva Raja <ishanku@gmail.com>
2020-01-31 21:54:03 -0500

    Update on main.py

--- main.py
+++ main.py
@@ -11,13 +11,21 @@ from config import bucketname
 FileHospitalGeneralData="MedicareDataAnalysis/HospitalData/Hospital General Information.csv"
 
 class DataLoader():
-    def LoadFile(file):
-        s3 = boto3.client('s3',
-        aws_access_key_id=awskey,
-        aws_secret_access_key=secret)
-        # Call S3 to list current buckets
-        response = s3.list_buckets()
-	# Hi Siva, In the below line, you hardcoded the Key value to be a the file handle object. It should be Key=file (the attribute which you are passing in the LoadFile method)#
-        Object_Read=s3.get_object(Bucket=bucketname,Key=FileHospitalGeneralData)
-        output_data= pd.read_csv(Object_Read['Body'])
-        return output_data
+	def LoadFile(file):
+		s3 = boto3.client('s3',aws_access_key_id=awskey,aws_secret_access_key=secret)
+		# Call S3 to list current buckets
+		response = s3.list_buckets()
+		Object_Read=s3.get_object(Bucket=bucketname,Key=file)
+		output_data= pd.read_csv(Object_Read['Body'])
+		return output_data
+
+class DataLoader1():
+	def LoadFile(file):
+		s3 = boto3.client('s3',aws_access_key_id=awskey,aws_secret_access_key=secret)
+		# Call S3 to list current buckets
+		response = s3.list_buckets()
+		#print (response)
+		Object_Read=s3.get_object(Bucket=bucketname,Key=file)
+		output_data= pd.read_csv(Object_Read['Body'])
+		#return output_data
+		print (output_data.head())
