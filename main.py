0a90eb301c610d28dd7e0ec45baf5a843f5be5a9
RRN-DATA <roopanagilla@gmail.com>
2020-01-31 20:30:57 -0500

    Roopa_class_file

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ class DataLoader():
         aws_secret_access_key=secret)
         # Call S3 to list current buckets
         response = s3.list_buckets()
+	# Hi Siva, In the below line, you hardcoded the Key value to be a the file handle object. It should be Key=file (the attribute which you are passing in the LoadFile method)#
         Object_Read=s3.get_object(Bucket=bucketname,Key=FileHospitalGeneralData)
         output_data= pd.read_csv(Object_Read['Body'])
         return output_data
